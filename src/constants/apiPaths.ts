b1b947076ce6727eff64648f92637385b1a7af2a
Kanstantsin Kulak <kkulak24@gmail.com>
2020-11-15 23:12:29 +0300

    feat: update import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://4up6kagkma.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://4up6kagkma.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://s8j3yuu37a.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vagr5e9505.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://4up6kagkma.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
